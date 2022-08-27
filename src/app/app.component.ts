179a34f530c8ccda650408ff14ac0ef5264f9dca
Lindos Mahadi <lindosmahadi@gmail.com>
2022-08-27 11:52:20 +0600

    Material form SetUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,10 @@ export class AppComponent {
 
   constructor(public dialog: MatDialog) { }
 
+
   openDialog() {
     this.dialog.open(DialogComponent, {
-      width: '50%'
+      width: '30%'
     });
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +30,13 @@ import { DialogComponent } from './dialog/dialog.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
